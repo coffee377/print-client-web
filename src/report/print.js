78a74c9f22b6595d717111f0e46fd3a00331c0c2
Wu Yujie <coffee377@dingtalk.com>
2018-12-25 18:05:15 +0800

    print tip && browser start client

--- src/report/print.js
+++ src/report/print.js
@@ -1,25 +1,30 @@
 import io from 'socket.io-client';
+import { message } from 'antd';
 import { getReportSession } from './query';
 import { SHOW_PRINT_TIP } from './base';
-// import {message} from 'antd';
-// message.config({
-// 	top: 100,
-// 	duration: 2,
-// 	maxCount: 3,
-// });
-// const printing = () => {
-// 	message.loading('正在打印...', 3);
-// 	// Dismiss manually and asynchronously
-// 	// setTimeout(hide, 2500);
-// };
-//
-// const printSuccess = () => {
-// 	message.success('打印完成', 2);
-// };
-//
-// const printError = (msg) => {
-// 	message.error(msg, 2);
-// };
+
+message.config({
+	top: 80,
+	duration: 3,
+	maxCount: 3
+});
+const printing = () => {
+	message.loading('正在打印...');
+};
+
+const printSuccess = result => {
+	if (result.success) {
+		message.success(result.message);
+	} else {
+		message.error(result.message);
+	}
+};
+
+const printError = result => {
+	if (result.success) {
+		message.error(result.message);
+	}
+};
 
 /**
  * Module variable.
@@ -27,8 +32,6 @@ import { SHOW_PRINT_TIP } from './base';
 const REPORT_SOCKET_URL = 'http://localhost:10227';
 
 const EVENT_TYPE_CHECKING = 'typeChecking';
-const EVENT_GET_CONFIG_DATA = 'getConfigData';
-const EVENT_SET_CONFIG_DATA = 'setConfigData';
 const EVENT_BEFORE_PRINT = 'beforePrint';
 const EVENT_PRINT = 'print';
 const EVENT_PREVIEW = 'preview';
@@ -37,33 +40,11 @@ const EVENT_ERROR_OCCURS = 'errorOccurs';
 
 let isLoadingPrint = false;
 let printSocket = io(REPORT_SOCKET_URL);
-let printSocketId = printSocket.id;
+let printSocketId;
 
 /**
  * Module dependencies.
  */
-
-/**
- * 启动打印客户端
- * @returns {boolean} 启动客户端是否成功
- */
-function startPrintPlus() {
-	// TODO: 2018/12/14 0014 9:07 启动客户端
-	debugger;
-	printSocket.connect();
-	if (printSocket && printSocket.connected) {
-		console.log('The PrintPlus client is started');
-		alert('PrintPlus启动成功');
-		isLoadingPrint = true;
-	} else {
-		debugger;
-		// printSocket.destroy();
-		// printSocket = null;
-		isLoadingPrint = false;
-		console.warn('The PrintPlus client boot failed');
-		alert('PrintPlus启动失败');
-	}
-}
 
 /**
  * 打印
@@ -84,6 +65,15 @@ function reportPreview(reportId, previewConfig) {
 }
 
 /**
+ * 浏览器启动本地打印客户端
+ * @param args 启动参数
+ */
+function protocolUrl(args) {
+	debugger;
+	window.location.href = `PrintPlus://${args}`;
+}
+
+/**
  * 根据报表ID 执行报表
  * @param {String} reportId 报表ID
  * @param {String} reportServer 报表服务地址
@@ -97,58 +87,61 @@ function reportPrintOperation(reportId, reportServer, quietPrint = true) {
 	}
 	// 点击打印时的实际配置
 	const config = { sessionID: session, quietPrint, url: reportServer };
+	debugger;
 	if (printSocket == null) {
 		printSocket = io(REPORT_SOCKET_URL);
 	} else {
 		printSocket.removeAllListeners();
 	}
+	debugger;
 	isLoadingPrint = printSocket.connected;
 	if (!isLoadingPrint) {
-		alert('PrintPlus未启动');
-		startPrintPlus();
+		protocolUrl();
 	}
-	if (isLoadingPrint) {
-		printSocket.on('connect', () => {
-			printSocketId = printSocket.id;
-			console.warn(`与打印客户端[${printSocketId}]建立连接`);
-		});
-		printSocket.on('disconnect', () => {
-			debugger;
-			console.warn(`与打印客户端[${printSocketId}]失去连接`);
-		});
-		/* 获取配置 */
-		printSocket.emit(EVENT_TYPE_CHECKING);
-		printSocket.on(EVENT_BEFORE_PRINT, () => {
-			if (SHOW_PRINT_TIP && quietPrint) {
-				alert('开始打印');
-				// printing();
+	printSocket.on('connect', () => {
+		printSocketId = printSocket.id;
+		isLoadingPrint = true;
+		console.warn(`与打印客户端[${printSocketId}]建立连接`);
+	});
+	printSocket.on('disconnect', () => {
+		debugger;
+		isLoadingPrint = false;
+		console.warn(`与打印客户端[${printSocketId}]失去连接`);
+	});
+	/* 触发事件类型检测 */
+	printSocket.emit(EVENT_TYPE_CHECKING, JSON.stringify(config));
+	/* 打印前事件 */
+	printSocket.on(EVENT_BEFORE_PRINT, () => {
+		debugger;
+		if (SHOW_PRINT_TIP && quietPrint) {
+			printing();
+		}
+	});
+	printSocket.on(EVENT_PRINT, res => {
+		debugger;
+		const data = res;
+		if (config.sessionID) {
+			config.url += `?sessionID=${config.sessionID}&op=fr_applet&cmd=print`;
+			for (const configKey in config) {
+				data[configKey] = config[configKey];
 			}
-			printSocket.emit(EVENT_BEFORE_PRINT, JSON.stringify(config));
-		});
-		printSocket.on(EVENT_PRINT, data => {
-			const tmpData = data;
-			if (config.sessionID) {
-				config.url += `?sessionID=${config.sessionID}&op=fr_applet&cmd=print`;
-				for (const configKey in config) {
-					tmpData[configKey] = config[configKey];
-				}
-				print(tmpData);
-			}
-		});
-		printSocket.on(EVENT_AFTER_PRINT, data => {
-			if (SHOW_PRINT_TIP && quietPrint) {
-				alert('打印结束');
-				// printSuccess();
-			}
-		});
-		printSocket.on(EVENT_PREVIEW, data => {
-			reportPreview(reportId, data);
-			console.log(`打印预览界面${JSON.stringify(data)}`);
-		});
-		printSocket.on(EVENT_ERROR_OCCURS, data => {
-			alert(`发生错误：${JSON.stringify(data)}`);
-		});
-	}
+			print(data);
+		}
+	});
+	printSocket.on(EVENT_AFTER_PRINT, res => {
+		debugger;
+		if (SHOW_PRINT_TIP && quietPrint) {
+			printSuccess(res);
+		}
+	});
+	printSocket.on(EVENT_PREVIEW, res => {
+		debugger;
+		reportPreview(reportId, res);
+		console.log(`打印预览界面${JSON.stringify(res)}`);
+	});
+	printSocket.on(EVENT_ERROR_OCCURS, res => {
+		printError(res);
+	});
 }
 
 /**
