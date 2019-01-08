f7dd43a52bc8011f0e0cb56061e574deef9138fd
Wu Yujie <coffee377@dingtalk.com>
2019-01-08 11:59:22 +0800

    test

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from './TodoList';
-import '../../public/html/app.html';
 import './style.less';
 
 const App = () => (
